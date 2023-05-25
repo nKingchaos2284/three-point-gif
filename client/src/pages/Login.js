fb87da24ad6cb746b0cd47e942a148e7f8febdc4
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-05-24 20:35:15 -0700

    Created Login.js
    Created the Login page structure for the application

---
+++ client/src/pages/Login.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/api/login', { email, password });
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.targe.value)}
+                />
+                <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
