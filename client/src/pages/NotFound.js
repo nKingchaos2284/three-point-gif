aa4695c662bb4c2d802d45991ecaf85afcfc8f49
Jose Mauricio Cabrera Elias <jmcabrerae@gmail.com>
2023-05-24 19:41:32 -0700

    minor changes in pages

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h1>404 - Page Not Found</h1>
-      <p>The requested page does not exist.</p>
+      <p>The requested page does not exist. ABORTING...</p>
     </div>
   );
 };
