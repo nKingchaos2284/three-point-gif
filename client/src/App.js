0f31f24c565eeceae1438ab5f8060a3ca6349d4f
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-05-24 20:37:29 -0700

    Updated App.js
    Updated the App.js file to include the newly added Login, Logout and Sign Up pages files for routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Search from './pages/Search';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import SignUp from './pages/SignUp';
 import NotFound from './pages/NotFound';
 
 const App = () => {
@@ -11,6 +14,9 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/search" component={Search} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/signup" component={SignUp} />
           <Route component={NotFound} />
         </Switch>
       </div>
