dfa1d46f541bb86fe5a7550daf58cce7b48692ab
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-05-24 20:34:23 -0700

    Created SignUp.js
    Created Sign Up page structure for application

---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/api/signup', { email, password });
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Sign Up To 'In A GIFFY'!</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+};
+
+export default Signup;
