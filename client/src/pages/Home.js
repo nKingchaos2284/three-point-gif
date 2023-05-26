81c18f37a43f1c8ce5b0185987e78bf4c981802a
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-05-25 21:11:50 -0700

    Updated Home.js
    Updated the Home.js to import the auth.js and utilize authentication

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,36 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import AuthService from '../utils/auth';
 
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const history = useHistory();
+  const isAuthenticated = AuthService.isLoggedIn();
 
   const handleSearch = (e) => {
     e.preventDefault();
     history.push(`/search/${searchTerm}`);
   };
 
+  const handleLogout = () => {
+    AuthService.logout();
+    history.push('/login');
+  };
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>In A GIFFY</h1>
       <form onSubmit={handleSearch}>
         <input
           type="text"
-          placeholder="Search term"
+          placeholder="Search for a GIF here!"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
         <button type="submit">Search</button>
       </form>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-      <Link to="/logout">
-        <button>Logout</button>
-      </Link>
-      <Link to="/signup">
-        <button>Sign Up</button>
-      </Link>
+      {isAuthenticated ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
+        <>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/signup">
+            <button>Sign Up</button>
+          </Link>
+        </>
+      )}
     </div>
   );
 };
