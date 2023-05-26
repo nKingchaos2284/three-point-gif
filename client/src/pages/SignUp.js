d43bd17cdc4ddd72d4cee40314597b7a2204d02a
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-05-25 18:44:58 -0700

    Updated SignUp.js
    Updated the SignUp.js file to be able to utilize the mutations.js file

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,40 +1,46 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import { useMutation } from '@apollo/client';
+import { SIGNUP_MUTATION } from '../utils/mutations';
 
-const Signup = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('/api/signup', { email, password });
-            console.log(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const [signup, { loading, error }] = useMutation(SIGNUP_MUTATION);
 
-    return (
-        <div>
-            <h1>Sign Up To 'In A GIFFY'!</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Sign Up</button>
-            </form>
-        </div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signup({ variables: { email, password } })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+    </div>
+  );
 };
 
-export default Signup;
+export default SignUp;
