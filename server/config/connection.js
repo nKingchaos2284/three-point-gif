d3952664fbe2190e9e0d613ded42315aaf3ea5e8
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-05-31 21:22:28 -0700

    Updated connection.js
    Updated the file to work with MERN application

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,21 +1,6 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-let sequelize;
+mongoose.connect(process.env.MONGODB_URI);
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
-
-module.exports = sequelize;
+module.exports = mongoose.connection;
