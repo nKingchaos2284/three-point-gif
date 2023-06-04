f6af5a06a292d1b850f00c4473e375a667f80e8c
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-06-04 11:35:26 -0700

    Updated userRoutes.js (#18)

--- server/controllers/userRoutes.js
+++ server/controllers/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User } = require("../models");
 
 // creating a new user
 router.post("/signup", async (req, res) => {
