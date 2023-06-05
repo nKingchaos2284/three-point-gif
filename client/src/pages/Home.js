c9627d1000230c8b32997cc2110d9e00afeb8298
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-06-04 17:47:14 -0700

    Updated Home.js
    Added token storage and a link to the website h1

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import AuthService from "../utils/auth";
@@ -6,35 +6,48 @@ import AuthService from "../utils/auth";
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const history = useHistory();
-  const isAuthenticated = AuthService.isLoggedIn();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setIsLoggedIn(AuthService.isLoggedIn());
+  }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
-
+  
     if (searchTerm.trim() === "") {
       return;
     }
-
+  
+    const token = localStorage.getItem('token');
+    
     axios
-      .post("/search", { "giphy-query": searchTerm })
+      .post("/api/search", { "giphy-query": searchTerm }, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
       .then((response) => {
         console.log(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
-
+  
     setSearchTerm("");
   };
 
   const handleLogout = () => {
     AuthService.logout();
+    setIsLoggedIn(false);
     history.push("/login");
   };
 
   return (
     <header>
+      <Link to="/">
       <h1>In A GIFFY</h1>
+      </Link>
       <form className="searchbar-form" onSubmit={handleSearch}>
         <input
           type="text"
@@ -44,7 +57,7 @@ const Home = () => {
         />
         <button type="submit">Search</button>
       </form>
-      {isAuthenticated ? (
+      {isLoggedIn ? (
         <button onClick={handleLogout}>Logout</button>
       ) : (
         <>
